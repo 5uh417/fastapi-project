662dd51f2c800373b54f5a9ea3194399d9143892
smirza <smirza@mac-workstation>
2021-06-11 22:21:14 +0530

    Implement in memory DB

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,9 +1,10 @@
 from models.validation_error import ValidationError
-from typing import Optional
+from typing import List, Optional
 import fastapi
 from fastapi.params import Depends
 from models.location import Location
-from services import openweather_service
+from models.reports import Report, ReportSubmittal
+from services import openweather_service, report_service
 
 router = fastapi.APIRouter()
 
@@ -19,3 +20,17 @@ async def weather(loc: Location = Depends(), units: Optional[str] = "metric"):
         return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
     except Exception as x:
         return fastapi.Response(content=str(x), status_code=500)
+
+
+@router.get("/api/reports", name="all_reports")
+async def reports_get() -> List[Report]:
+    # await report_service.add_report("A", Location(city="Portland"))
+    # await report_service.add_report("B", Location(city="NYC"))
+    return await report_service.get_reports()
+
+
+@router.post("/api/reports", name="add_reports", status_code=201)
+async def reports_post(report_submittal: ReportSubmittal) -> Report:
+    d = report_submittal.description
+    loc = report_submittal.location
+    return await report_service.add_report(d, loc)
